61704b61787a658bc0f8330a09eb03dba7c16585
Pete Corey <petecorey@gmail.com>
2016-08-01 12:12:21 -0400

    # Final Refactor
    To make things a little more readable, I decided to move the `undefined`
    check into a guard, rather than including it withing the comparison
    logic of our `zipWith` function.
    
    After implementing this refactor, our tests still pass.

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ Array.prototype.sameStructureAs = function(other) {
         return false;
     }
     let comparisons = _.zipWith(this, other, (a, b) => {
+        if (_.isUndefined(a) || _.isUndefined(b)) {
+            return false;
+        }
         let bothArrays = _.isArray(a) && _.isArray(b);
-        let bothDefined = !_.isUndefined(a) && !_.isUndefined(b);
-        let bothNot = bothDefined && !_.isArray(a) && !_.isArray(b);
+        let bothNot = !_.isArray(a) && !_.isArray(b);
         return (bothArrays && a.sameStructureAs(b)) || bothNot;
     });
     return _.every(comparisons);
